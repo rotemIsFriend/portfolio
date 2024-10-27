100d04265f14c82b09ca11edf0d82040feebe753
Rotem <rotman1999@gmail.com>
2024-10-27 15:16:56 +0200

    Switched to pytz for timezone management

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+from pytz import timezone
+TIME_ZONE = timezone('UTC')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
