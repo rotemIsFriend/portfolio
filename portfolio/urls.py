be5bde05afdafc95f78e903bcf67fbc72436d4bb
Rotem <rotman1999@gmail.com>
2024-10-28 13:19:25 +0200

    Add empty favicon.ico

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def favicon_view(request):
+    return HttpResponse(status=204)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', favicon_view),
     path('', include('myPortfolio.urls')),
 ]
 
